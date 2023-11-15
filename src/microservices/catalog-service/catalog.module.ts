0cc2b8bab605113903389c04f087916494c8f74c
ItzCornflakez <Dikesalex@gmail.com>
2023-11-15 09:22:29 +0100

    first commit

---
+++ src/microservices/catalog-service/catalog.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { CatalogController } from './catalog.controller';
+
+@Module({
+    controllers: [CatalogController],
+  imports: [],
+  providers: []
+})
+export class CatalogModule {}
---
+++ src/microservices/order-management-service/order.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { OrderController } from './order.controller';
+
+@Module({
+    controllers: [OrderController],
+  imports: [],
+  providers: []
+})
+export class OrderModule {}
