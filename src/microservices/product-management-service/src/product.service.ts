4d2fc19ce64adc30d382c1c275618abb0cb3bdf1
ItzCornflakez <Dikesalex@gmail.com>
2023-12-06 11:42:04 +0100

    Done with product service for now

--- src/microservices/product-management-service/src/product.service.ts
+++ src/microservices/product-management-service/src/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Category, Prisma, Product } from '@prisma/client';
+import { Category, Inventory, Prisma, Product } from '@prisma/client';
 import { PrismaService } from './prisma.service';
 
 @Injectable()
@@ -22,6 +22,65 @@ export class ProductsService {
     });
   }
 
+  async inventory(
+    inventoryWhereUniqueInput: Prisma.InventoryWhereUniqueInput,
+  ): Promise<Inventory | null> {
+    return this.prisma.inventory.findUnique({
+      where: inventoryWhereUniqueInput,
+    });
+  }
+
+  async products(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.ProductWhereUniqueInput;
+    where?: Prisma.ProductWhereInput;
+    orderBy?: Prisma.ProductOrderByWithRelationInput;
+  }): Promise<Product[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.product.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async categories(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.CategoryWhereUniqueInput;
+    where?: Prisma.CategoryWhereInput;
+    orderBy?: Prisma.CategoryOrderByWithRelationInput;
+  }): Promise<Category[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.category.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async inventories(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.InventoryWhereUniqueInput;
+    where?: Prisma.InventoryWhereInput;
+    orderBy?: Prisma.InventoryOrderByWithRelationInput;
+  }): Promise<Inventory[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.inventory.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
   async createProduct(data: Prisma.ProductCreateInput): Promise<Product> {
     return this.prisma.product.create({
       data,
@@ -33,4 +92,62 @@ export class ProductsService {
       data,
     });
   }
+
+  async createInventory(data: Prisma.InventoryCreateInput): Promise<Inventory> {
+    return this.prisma.inventory.create({
+      data,
+    });
+  }
+
+  async updateProduct(params: {
+    where: Prisma.ProductWhereUniqueInput;
+    data: Prisma.ProductUpdateInput;
+  }): Promise<Product> {
+    const { where, data } = params;
+    return this.prisma.product.update({
+      data,
+      where,
+    });
+  }
+
+  async updateCategory(params: {
+    where: Prisma.CategoryWhereUniqueInput;
+    data: Prisma.CategoryUpdateInput;
+  }): Promise<Category> {
+    const { where, data } = params;
+    return this.prisma.category.update({
+      data,
+      where,
+    });
+  }
+
+  async updateInventory(params: {
+    where: Prisma.InventoryWhereUniqueInput;
+    data: Prisma.InventoryUpdateInput;
+  }): Promise<Inventory> {
+    const { where, data } = params;
+    return this.prisma.inventory.update({
+      data,
+      where,
+    });
+  }
+
+  async deleteProduct(where: Prisma.ProductWhereUniqueInput): Promise<Product> {
+    return this.prisma.product.delete({
+      where,
+    });
+  }
+
+  async deleteCategory(where: Prisma.CategoryWhereUniqueInput): Promise<Category> {
+    return this.prisma.category.delete({
+      where,
+    });
+  }
+
+  async deleteInventory(where: Prisma.InventoryWhereUniqueInput): Promise<Inventory> {
+    return this.prisma.inventory.delete({
+      where,
+    });
+  }
+  
 }
