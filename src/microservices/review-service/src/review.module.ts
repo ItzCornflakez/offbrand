15188dec5846ca7b5c35611025931932df44592a
Chefenz <pingispontus@gmail.com>
2023-12-13 00:32:39 +0100

    implemnted review-service not tested but followed in a simmilar fashion to product-management-service

--- src/microservices/review-service/src/review.module.ts
+++ src/microservices/review-service/src/review.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReviewController } from './review.controller';
+import { ReviewService } from './review.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   controllers: [ReviewController],
   imports: [],
-  providers: [],
+  providers: [ReviewService, PrismaService],
 })
 export class ReviewModule {}
