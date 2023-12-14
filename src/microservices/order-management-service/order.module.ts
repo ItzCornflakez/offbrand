abe6152f77e11c0b7d5240f33070e66d22936434
Emil Nyberg <emilnyberg1998@gmail.com>
2023-12-14 09:43:22 +0100

    Refresh .gitignore

--- src/microservices/order-management-service/order.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OrderController } from './order.controller';
-
-@Module({
-  controllers: [OrderController],
-  imports: [],
-  providers: [],
-})
-export class OrderModule {}
